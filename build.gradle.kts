acc358fcfc1cc08d96c38cbd83a5d69798dd0f23
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-09-30 20:17:54 +0100

    Remove GUI Editor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,21 @@
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("application")
-    id("io.github.file5.guidesigner") version "1.0.2"
 }
 
 group = "com.nospawnn"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven {
-        url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies")
-    }
     mavenCentral()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
 
-    runtimeOnly("com.jetbrains.intellij.java:java-gui-forms-rt:242.22855.106")
     val flatlaf = "3.5.1"
     implementation("com.formdev:flatlaf:${flatlaf}")
     implementation("com.formdev:flatlaf:${flatlaf}.:linux-x86_64@so")
     implementation("com.formdev:flatlaf:${flatlaf}:windows-x86_64@dll")
-}
-
-application {
-    mainClass = "com.nospawnn.MainKt"
 }
 
 tasks.test {
